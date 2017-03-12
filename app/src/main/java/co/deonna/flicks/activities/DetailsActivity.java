509a86f2b31e86eac2b14673199f6ad82f1d40a0
Deonna Hodges <dnhodges@gmail.com>
2017-03-11 16:18:09 -0800

    fix(flicks): Fixed issue with Design view - the theme was not resolving.

---
+++ app/src/main/java/co/deonna/flicks/activities/DetailsActivity.java
@@ -0,0 +1,12 @@
+package co.deonna.flicks.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
